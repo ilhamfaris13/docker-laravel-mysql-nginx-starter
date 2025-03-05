8437ad8c55e383d0380ebb736e4b7a068421dbd8
Muhammad Ilham Faris <53896024+ilhamfaris13@users.noreply.github.com>
2025-03-05 10:39:31 +0700

    login

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -1,8 +1,17 @@
 <?php
 
 namespace Database\Seeders;
+//use Illuminate\Database\Seeder;
+use App\Models\Kategori;
+use App\Models\Divisi;
+use App\Models\Karyawan;
+use App\Models\Barang;
+use App\Models\Peminjaman;
+use App\Models\Pengembalian;
+use App\Models\Maintenance;
+use App\Models\LogAktivitas;
 
-use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,14 +19,19 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            "name" => "Test User",
-            "email" => "test@example.com",
-        ]);
+        // Buat data dummy
+        //Kategori::factory(5)->create();
+        Divisi::factory(3)->create();
+        Karyawan::factory(10)->create();
+        Barang::factory(20)->create();
+        Peminjaman::factory(15)->create();
+        Pengembalian::factory(10)->create();
+       // Maintenance::factory(8)->create();
+        LogAktivitas::factory(30)->create();
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
-    return view("welcome");
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+require __DIR__.'/auth.php';
+
+use App\Http\Controllers\BarangController;
+
+Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
+Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
+Route::post('/barang', [BarangController::class, 'store'])->name('barang.store');
+Route::get('/barang/{id}', [BarangController::class, 'show'])->name('barang.show');
+Route::get('/barang/{id}/edit', [BarangController::class, 'edit'])->name('barang.edit');
+Route::put('/barang/{id}', [BarangController::class, 'update'])->name('barang.update');
+Route::delete('/barang/{id}', [BarangController::class, 'destroy'])->name('barang.destroy');
+Route::get('/barang/{id}/print', [BarangController::class, 'print'])->name('barang.print');
+Route::get('/barang/{id}/pdf', [BarangController::class, 'pdf'])->name('barang.pdf');
